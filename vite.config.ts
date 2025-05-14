132ac24c7e1d3fca0db16074c11da30e1d01c3cf
sebastian <jmendozagon@my.smccd.edu>
2025-05-14 06:21:12 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/DGME168-Sebastian/',
+  base: process.env.NODE_ENV === 'production' ? '/DGME168-Sebastian/' : '/',
   server: {
     host: '::', // Allow access on the local network (optional)
     port: 8080,
