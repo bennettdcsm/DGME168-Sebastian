efde6bdf28bd23bb489c33a8f8446fe27b7d854a
Sebástian <sebastian@JMs-MacBook-Air.local>
2025-05-18 20:38:27 -0700

    Fix routing basename for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,12 +15,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/DGME168-Sebastian">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/other-work" element={<OtherWork />} />
           <Route path="/about" element={<About />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
